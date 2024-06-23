0b304f96a343696b3ac16548e56fb8a4496513cd
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-23 12:14:54 -0700

    two sum

--- src/ArraysAndHashing/Main.java
+++ src/ArraysAndHashing/Main.java
@@ -1,5 +1,7 @@
 package ArraysAndHashing;
 
+import java.util.Arrays;
+
 public class Main {
 
 
@@ -7,13 +9,17 @@ public class Main {
 
         HasDuplicate duplicate = new HasDuplicate();
         IsAnagram anagram = new IsAnagram();
+        TwoSum twoSum = new TwoSum();
 
         int[] nums = {1,2,3,4,5};
+        int[] sums = {3,4,5,6};
+
         String string1 = "racecar";
         String string2 = "carrace";
 
         System.out.println(anagram.isAnagram(string1, string2));
         System.out.println(duplicate.hasDuplicate(nums));
+        System.out.println(Arrays.toString(twoSum.twoSum(sums, 7)));
 
     }
 }
