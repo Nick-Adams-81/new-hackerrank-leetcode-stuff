9122ae74591d21e7f01b92864f00852f4fdc1601
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-23 12:06:26 -0700

    is anagram

--- src/ArraysAndHashing/Main.java
+++ src/ArraysAndHashing/Main.java
@@ -4,8 +4,15 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         HasDuplicate duplicate = new HasDuplicate();
+        IsAnagram anagram = new IsAnagram();
+
         int[] nums = {1,2,3,4,5};
+        String string1 = "racecar";
+        String string2 = "carrace";
+
+        System.out.println(anagram.isAnagram(string1, string2));
         System.out.println(duplicate.hasDuplicate(nums));
 
     }
